76a41a32fb67221b1ed4784e92e2f9bdfd3dd460
Oscar Gonzalez <gonzale03@gmail.com>
2024-04-13 16:56:41 -0500

    crear variables y modelo de agrupamiento

--- Actividad_4.py
+++ Actividad_4.py
@@ -9,3 +9,18 @@ ruta_csv = "C:\\Users\\wilme\\Pictures\\siza\\transporte_masivo.csv"
 
 # Cargar datos desde un archivo CSV
 datos = pd.read_csv(ruta_csv)
+
+# Preprocesamiento de datos
+X = datos[['distancia', 'tiempo', 'costo']]  # Características
+
+# Crear un modelo de agrupamiento (K-Means)
+kmeans = KMeans(n_clusters=10, random_state=42)
+kmeans.fit(X)
+
+# Asignar las etiquetas de los clusters a los datos
+datos['cluster'] = kmeans.labels_
+
+
+# Calcular el coeficiente de silueta
+score_silueta = silhouette_score(X, datos['cluster'])
+print(f"Coeficiente de Silueta: {score_silueta}")
